7a1ce256b74f1d9998fccffffb3b1b54bbcd32ce
Shawn McManus <xsvfan76@gmail.com>
2020-01-09 00:20:34 -0700

    fixed contact form able to email now

--- portfolio/assets/js/email.js
+++ portfolio/assets/js/email.js
@@ -1,1 +1,34 @@
-console.log('it works')
+console.log('it works');
+
+$(document).ready(function() {
+  $('.primary').click(function(event) {
+    console.log('Clicked Button');
+
+    var email = $('.email').val();
+    var name = $('.name').val();
+    var message = $('.message').val();
+    var statusElm = $('.status');
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      //   statusElm.append('<div>Email is valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>Email is incorrect</div>');
+    }
+
+    if (name.length > 2) {
+      //   statusElm.append('<div>name is valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>name is not valid</div>');
+    }
+
+    if (message.length >= 10) {
+      //   statusElm.append('<div>Message is valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>Message is not valid</div>');
+    }
+  });
+});
